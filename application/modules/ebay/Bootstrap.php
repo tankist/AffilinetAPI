08d214afd0b902af17022650582d262f548953f6
Victor <stalin1945@gmail.com>
2011-08-22 15:43:10 +0300

    Added: default route

--- application/modules/ebay/Bootstrap.php
+++ application/modules/ebay/Bootstrap.php
@@ -10,7 +10,7 @@ class Ebay_Bootstrap extends Zend_Application_Module_Bootstrap
         /**
          * @var Zend_Controller_Router_Rewrite $router
          */
-        $router = Zend_Controller_Front::getInstance()->getRouter();
+        $router = $this->getResource('frontController')->getRouter();
 
         $router->addRoute('ebay',
                           new Zend_Controller_Router_Route('ebay/testebay/:page',
